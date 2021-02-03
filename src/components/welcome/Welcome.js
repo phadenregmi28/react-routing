9e4e8b7fabcea8ba35f1c9abb1108bb46a2f47b4
phadenregmi28 <69169756+phadenregmi28@users.noreply.github.com>
2021-02-02 20:50:45 -0500

    first commit

---
+++ src/components/welcome/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Welcome(props){
+    console.log(props);
+
+let name = props.match.params.name || props.name;
+    return(
+     <div className="Welcome"> Welcome, {name} </div>
+    );
+}
+
+
+export default Welcome;
